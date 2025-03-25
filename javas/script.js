2c88a6edc4d0c58a252b756efe4eaa2e054efae7
Dear-deer1 <alenazlobina112@gmail.com>
2025-03-25 21:01:39 +0300

    25.03

---
+++ javas/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollButton = document.getElementById("scrollButton");
+    const section2 = document.getElementById("section2");
+
+    scrollButton.addEventListener("click", function () {
+        section2.scrollIntoView({ behavior: "smooth" });
+    });
+});
